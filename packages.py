1c7dd916e68842b8c42c24fd0a68222281eaed67
Divya Kumar Regmi <erdivyakumar@gmail.com>
2025-05-22 09:20:16 +0545

    packages.py

---
+++ packages.py
@@ -0,0 +1,18 @@
+#python -m pip list 
+# package in python is way to organize related modules (python files) together in a directory
+#A package is simply a folder containing an __init__.py file (can be empty) and one or more module files.
+#Packages help you structure your code and avoid name conflicts.
+
+
+
+#python -m pip freeze > requirement.txt 
+#pip show pandas
+#pip list
+#pip install pandas
+#python -m install -r requirement.txt
+#python -m venv venv (to create virtual environment)
+
+
+
+
+
